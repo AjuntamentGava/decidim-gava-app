d05085593d989429a29469e4bd81686b5a96ea40
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2024-09-27 18:56:19 +0200

    Add pending crons (#18)
    * Add pending crons
    
    * Add validate_migrations CI workflow
    
    * Remove old configuration
    
    * Adapt validate_migrations CI

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,8 +6,16 @@ every 1.day, at: "3:00 am" do
   rake "decidim_meetings:clean_registration_forms"
 end
 
+every :monday, at: "3:30 am" do
+  rake "tmp:clear"
+end
+
 every 1.day, at: "4:00 am" do
   rake "decidim:reminders:all"
+end
+
+every :sunday, at: "4:30 am" do
+  rake "decidim:delete_data_portability_files"
 end
 
 every 1.day, at: "5:00 am" do
@@ -16,8 +24,4 @@ end
 
 every 1.day, at: "6:00 am" do
   rake "decidim:open_data:export"
-end
-
-every :sunday, at: "4:00 am" do
-  rake "decidim:delete_data_portability_files"
 end
