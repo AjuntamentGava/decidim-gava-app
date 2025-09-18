d6f7b9a3635a7ca2c83c74282d44a03c37f1b8f4
Laura J <75725233+laurajaime@users.noreply.github.com>
2025-09-18 09:14:39 +0200

    Add S3 to CSP (#22)
    * Add S3 to CSP
    
    * Update Ruby to 3.1.7 and cdtb to last version
    
    * Update Ruby in GHActions
    
    * Add S3 to frame and connect in CSP

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -52,9 +52,10 @@ Decidim.configure do |config|
 
   # Configure CSP
   config.content_security_policies_extra = {
-    "connect-src" => %w(https://*.here.com https://*.hereapi.com),
-    "img-src" => %w(https://*.here.com https://*.hereapi.com),
-    "script-src" => %w(https://www.googletagmanager.com)
+    "connect-src" => %w(https://*.here.com https://*.hereapi.com https://*.s3.eu-west-3.amazonaws.com),
+    "img-src" => %w(https://*.here.com https://*.hereapi.com https://*.s3.eu-west-3.amazonaws.com),
+    "script-src" => %w(https://www.googletagmanager.com),
+    "frame-src" => %w(https://*.s3.eu-west-3.amazonaws.com)
   }
 end
 
